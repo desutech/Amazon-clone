81bcf36b460f995e27b3b9d71fe16a6d2bdc8506
desutech <desaleyyazezew@gmail.com>
2024-11-23 01:28:40 +0300

    carousel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import { img } from './img/data'
+import classes from './Carousel.module.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
+function CarouselEffect() {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+        showStatus={false}
+      >
+        {
+          img.map((imageItemLink, index) => {
+            return <img src={imageItemLink} key={index} />
+          })
+        }
+
+
+      </Carousel>
+      <div className={classes.hero_img}>
+        ' '
+      </div>
+
+    </div>
+  )
+}
+
+export default CarouselEffect;
+
+
